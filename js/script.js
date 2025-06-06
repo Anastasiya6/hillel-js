03a3fe49d3c4e0c44916de8ee8450e39b2fabad8
Anastasiya06 <nastya06051981@gmail.com>
2025-06-06 15:07:08 +0300

    hw: the average of three numbers

---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let num1 = +prompt('Введіть перше число');
+let num2 = +prompt('Введіть друге число');
+let num3 = +prompt('Введіть третє число');
+
+alert(`Середнє арифметичне трьох чисел ${num1}+${num2}+${num3} = `+(num1 + num2 + num3)/3 );
