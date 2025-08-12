67be691eb7c464d074b38eacaf8d9ff59d22f781
Anastasiya06 <nastya06051981@gmail.com>
2025-08-12 10:22:32 +0300

    hw

---
+++ js/script.js
@@ -0,0 +1,15 @@
+class Navigation {
+
+}
+console.log(window.location.pathname );
+
+const li = document.querySelector('.nav-item');
+const link = document.querySelector('.nav-link');
+console.log(li);
+
+li.addEventListener("click", function (e) {
+
+    // const active = document.querySelector('.nav-link.active');
+    // document.active.classList.toggle('active');
+    // document.link.classList.toggle('active');
+});
