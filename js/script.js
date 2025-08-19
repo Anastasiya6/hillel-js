403685882452bd81d237aa317f97881f80257569
Anastasiya06 <nastya06051981@gmail.com>
2025-08-19 22:26:19 +0300

    hw

--- js/script.js
+++ js/script.js
@@ -1,15 +1,44 @@
 class Navigation {
 
+    constructor() {
+        this.list = document.querySelector('[data-list]');
+        this.list.addEventListener('click',this.update);
+        this.updateUrl(window.location.pathname);
+        window.addEventListener('popstate', () => {
+            this.updateActive(window.location.pathname);
+        });
+    }
+
+    update = (e) => {
+        e.preventDefault();
+        const url = e.target.getAttribute('href');
+        this.updateUrl(url);
+        this.updateActive(url);
+    }
+
+    updateActive(pathUrl) {
+        this.list.querySelectorAll('a').forEach(link => {
+            link.classList.toggle('active', link.getAttribute('href') === pathUrl);
+        });
+    }
+
+    updateUrl(url) {
+        const linkPath = new URL(url, window.location.origin).pathname;
+        history.pushState({state: linkPath}, '', linkPath);
+    }
+
 }
-console.log(window.location.pathname );
 
-const li = document.querySelector('.nav-item');
-const link = document.querySelector('.nav-link');
-console.log(li);
+const nav = new Navigation();
 
-li.addEventListener("click", function (e) {
-
-    // const active = document.querySelector('.nav-link.active');
-    // document.active.classList.toggle('active');
-    // document.link.classList.toggle('active');
-});
+// const list = document.querySelector('[data-list]');
+// //const link = document.querySelector('.navbar-nav');
+// //console.log(link);
+//
+// list.addEventListener("click", function (e) {
+//     console.log(e.target);
+//     console.log(window.location.pathname );
+//     //element.classList.add('new-class');
+//     const element = e.target;
+//     element.classList.toggle('active');
+// });
