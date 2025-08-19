3f0efb24bd06d7d00b2600f473c5b54857935b41
Anastasiya06 <nastya06051981@gmail.com>
2025-08-20 00:47:00 +0300

    hw

--- js/URLParser.js
+++ js/URLParser.js
@@ -31,9 +31,4 @@ class URLParser {
     }
 }
 
-const url = new URLParser("https://example.com/products/item?search=book&page=2");
-console.log(url.protocol);
-console.log(url.hostname);
-console.log(url.path);
-console.log(url.queryParams);
 export default URLParser
--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 import URLParser from "./URLParser.js";
+
+const url = new URLParser("https://example.com/products/item?search=book&page=2");
+console.log(url.protocol);
+console.log(url.hostname);
+console.log(url.path);
+console.log(url.queryParams);
