87d1dabfa0de22d20c6b3ebf6fbfcc8c4967a940
Anastasiya06 <nastya06051981@gmail.com>
2025-09-09 15:53:27 +0300

    hw

---
+++ js/app.js
@@ -0,0 +1,91 @@
+'use strict';
+
+function createUserInterface() {
+
+    const form = document.querySelector('[data-form]');
+
+    const submitBtn = form.querySelector('[type="submit"]');
+    submitBtn.setAttribute('disabled', 'disabled');
+    submitBtn.disabled = true;
+
+    const inputs = Array.from(form.querySelectorAll('input'));
+    const list = document.querySelector('[data-list=phoneList]')
+    list.addEventListener('click', ({target}) => {
+
+        if(target.closest('[data-contact-id]') === null) return;
+        const contactElement = target.closest('[data-contact-id]');
+        console.log(contactElement);
+        const contactId = parseInt(contactElement.getAttribute('data-contact-id'));
+        console.log(contactId);
+        const removeContact = dataBase.deleteData(contactId);
+        if(removeContact !== null) {
+            removedContact(contactElement);
+        }
+    });
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const {target} = e;
+
+        // Get data from the form
+        const data = inputs.reduce((acc, {name, value}) =>{
+            acc[name] = value;
+            return acc;
+        }, {});
+
+
+        target.reset()
+        const newData = dataBase.setData(data)
+        let wrapper = createWrapper(newData);
+        list.prepend(wrapper);
+    });
+
+    const removedContact = (element) => {
+        element.remove();
+    }
+
+    const createWrapper = (contact) => {
+        const wrapper = document.createElement('li');
+        wrapper.setAttribute('data-contact-id', contact.id);
+        wrapper.className = 'list-group-item d-flex align-items-center justify-content-between';
+        wrapper.innerHTML = `
+                <div class="text-black"><b>${contact.firstName} ${contact.lastName}</b></div>
+                <button class="btn btn-danger btn-sm" data-remove-contact>Delete</button>
+                </div>
+               `;
+        return wrapper;
+    }
+
+    const renderContacts = () => {
+        const data = dataBase.getData();
+
+        data.forEach((contact) => {
+            let wrapper = createWrapper(contact);
+            list.prepend(wrapper);
+        })
+        form.reset();
+    }
+
+    const disabledHandler = (e) => {
+        let isInputFilled = true;
+        for(let i = 0; i < inputs.length; i++) {
+            if(!inputs[i].value.trim().length) {
+                isInputFilled = false;
+                break;
+            }
+        }
+
+        if(isInputFilled) {
+            submitBtn.removeAttribute('disabled')
+            submitBtn.disabled = false;
+        } else {
+            submitBtn.setAttribute('disabled', 'disabled');
+            submitBtn.disabled = true;
+        }
+    }
+    renderContacts();
+    form.addEventListener('input', disabledHandler)
+}
+
+createUserInterface()
--- js/data.js
+++ js/data.js
@@ -1,9 +1,9 @@
 'use strict';
 
-function createModel() {
-
-    const DB_KEY = 'contact_book';
+function createDataBase() {
     const OBJECT_KEYS = ['firstName', 'lastName', 'phone'];
+    //const DB = [];
+
     const validateObject = (objectToValidate) => {
         if(typeof objectToValidate !== 'object') return false;
         const keysToValidate = Object.keys(objectToValidate);
@@ -21,48 +21,48 @@ function createModel() {
         return isValid;
     }
     const getData = () => {
-        const data = JSON.parse(localStorage.getItem(DB_KEY));
-
+        const data = JSON.parse(localStorage.getItem("phonebook"));
         if(data === null) return [];
         return data;
+        //return DB;
     }
     const setData = (data) => {
+
         // Object validation
         if(!data) return null;
+
         if(typeof data !== "object") return null;
+
         if(!validateObject(data)) return null;
 
-        // Getting data from localStorage
+        // Generate uniq ID
+        let id = 1
         const currentData = getData();
-
-        // Calculating ID
-        let id = 1;
-        if(currentData.length) {
-            id = currentData.at(-1).id;
-            id += 1
+        if(currentData.length > 0) {
+            id = currentData[currentData.length-1].id + 1
         }
 
-        // Saving data to localStorage
+        // Save data to database;
         const dataToSave = {...data, id};
-        currentData.push(dataToSave);
-        localStorage.setItem(DB_KEY, JSON.stringify(currentData));
+       // DB.push(dataToSave);
 
-        // Returning saved element
-        return getData().at(-1)
+        currentData.push(dataToSave);
+        localStorage.setItem("phonebook", JSON.stringify(currentData));
+        // return saved element
+        return currentData.at(-1);
     }
     const deleteData = (id) => {
+
         if(typeof id !== 'number') return null;
         const currentData = getData();
+        const contactIndex = currentData.findIndex((currentData) => id === currentData.id );
+        const removedData = currentData.splice(contactIndex, 1);
 
-        const userIndex = currentData.findIndex((singleUser) => id === singleUser.id );
-
-        if(userIndex === -1) return null;
-
-        const removedContact = currentData.splice(userIndex, 1)[0];
-        localStorage.setItem(DB_KEY, JSON.stringify(currentData));
-
-        return removedContact;
+        localStorage.setItem('phonebook', JSON.stringify(currentData));
+        if(!removedData.length) return null;
+        return removedData[0];
     }
+
 
     return {
         getData,
@@ -71,4 +71,4 @@ function createModel() {
     }
 }
 
-const dataBase = createModel();
+const dataBase = createDataBase();
