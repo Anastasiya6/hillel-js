1f5201432817dd316b6530d486d2fdbe177228a5
Anastasiya06 <nastya06051981@gmail.com>
2025-09-15 22:33:10 +0300

    hw

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+function App() {
+
+  return (
+    <>
+      <Header/>
+        <div className="container">
+          <Footer/>
+        </div>
+
+    </>
+  )
+}
+
+export default App
