777ba9ee1ff3dffa780636f24226fb208cfbddc0
Anastasiya06 <nastya06051981@gmail.com>
2025-09-15 22:31:59 +0300

    hw

---
+++ my-react-app/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+        <p className="text-center text-muted">© 2025 Anastasiya. All Rights Reserved.</p>
+    )
+}
+
+export default Footer
---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,33 @@
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <div className="container">
+                <a className="navbar-brand fw-bold" href="#">Anastasiya</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/about">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/skills">Skills</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/projects">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    )
+}
+
+export default Header
