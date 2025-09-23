92c55863597bbec45555a3ca06a0daa3c5fa08f0
Anastasiya06 <nastya06051981@gmail.com>
2025-09-23 09:25:24 +0300

    hw

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,25 @@
 import Button from "./components/Button.jsx";
 import Result from "./components/Result.jsx";
+import { useState } from 'react';
 
 function App() {
-
+    const [smiles, setSmiles] = useState(Array(4).fill(null));
+    function handleClick(i) {
+        const addSmiles = [...smiles];
+        addSmiles[i] = ++addSmiles[i];
+        setSmiles(addSmiles);
+    }
+    function showSquares() {
+        console.log(Math.max.apply(null, smiles));
+    }
   return (
- 
+
     <>
-      <Button id={1} smile={'😀'} count={0}/>
-      <Button id={2} smile={'😎'} count={0}/>
-      <Button id={3} smile={'😘'} count={0}/>
-      <Button id={4} smile={'😍'} count={0}/>
+      <Button id={1} smile={'😀'} count={0} value={smiles[0]} onButtonClick={() => handleClick(0)}/>
+      <Button id={2} smile={'😎'} count={0} value={smiles[1]} onButtonClick={() => handleClick(1)}/>
+      <Button id={3} smile={'😘'} count={0} value={smiles[2]} onButtonClick={() => handleClick(2)}/>
+      <Button id={4} smile={'😍'} count={0} value={smiles[3]} onButtonClick={() => handleClick(3)}/>
+        <button onClick={showSquares}>Показати squares</button>
         <Result />
     </>
   )
--- my-react-app/src/components/Button.jsx
+++ my-react-app/src/components/Button.jsx
@@ -3,22 +3,15 @@ import React from "react";
 class Button extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            count: 0,
-        };
-    }
-
-    handleClick = () => {
-        this.setState({ count: this.state.count + 1 });
     }
 
     render() {
 
         return (
             <div>
-                <button onClick={this.handleClick}>
-                    {this.props.img}
-                    Clicked {this.state.count} times
+                <button onClick={this.props.onButtonClick}>
+                    {this.props.smile}
+                    Clicked {this.props.value} times
                 </button>
             </div>
         );
